6eb815bcdc2758bebb8a81453bd15f28402352cb
Shane Orr <shane.j.orr@gmail.com>
2019-06-03 19:26:28 -0400

    create master dataset

---
+++ benefits_tables/master_dataset.R
@@ -0,0 +1,19 @@
+############################################################################
+#
+# This script combines the individual benefit datasets to create a master
+# csv dataset for plotting
+#
+##############################################################################
+
+library(tidyverse)
+
+file_dir <- "benefits_tables/tables/"
+
+master <- bind_rows(
+  list(
+    read_rds(str_c(file_dir, 'work_first.rds')),
+    read_rds(str_c(file_dir, 'made_up.rds')),
+    read_rds(str_c(file_dir, 'snap.rds'))
+  ))
+
+write_csv(master, "plots/benefits.csv")
