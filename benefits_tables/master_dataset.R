32eb2c46b02570dc74379fa75eaf4fc1db95a11e
Shane Orr <shane.j.orr@gmail.com>
2019-06-08 16:04:44 -0400

    complete wic

--- benefits_tables/master_dataset.R
+++ benefits_tables/master_dataset.R
@@ -15,7 +15,8 @@ master <- bind_rows(
     read_rds(str_c(file_dir, 'snap.rds')),
     read_rds(str_c(file_dir, 'child_care_subsidy.rds')),
     read_rds(str_c(file_dir, 'prek.rds')),
-    read_rds(str_c(file_dir, 'sec8.rds'))
+    read_rds(str_c(file_dir, 'sec8.rds')),
+    read_rds(str_c(file_dir, 'wic.rds'))
   )) %>%
   arrange(benefit, monthly_income, adults, children)
 
--- benefits_tables/wic.R
+++ benefits_tables/wic.R
@@ -5,12 +5,23 @@
 ###############################################################################
 
 library(tidyverse)
-head(wic)
+
 wic <- read_rds('benefits_tables/tables/base.rds')
 
 # average food costs per person in 2018 was 42.28
 # https://www.fns.usda.gov/pd/wic-program
-# assumes recipients get this amount
+# assume recipients get this amount
+# to get benefits, the children must be under 5
+# we'll assume that all children are under 5 except
+# for one child in the 3 child house
+# the mother also gets benefits, and we'll assume that in one
+# adult households the adult is the mother
+# given this, the amount of the benefit is the number of children plus one,
+# times 42.28, minus 42.28 if there are three children
+wic <- wic %>%
+  # only get WIC payment if family has kids
+  mutate(payment = ifelse(children > 0, (children + 1) * 42.28, 0),
+         payment = ifelse(children == 3, payment - 42.28, payment))
 
 # can receive wic up to 185% of fpl
 fpl <- read_rds('benefits_tables/tables/federal_poverty_guidelines.rds') %>%
@@ -25,7 +36,7 @@ wic <- wic %>%
   left_join(fpl, by='size') %>%
   # set payment to 0 if income is greater than 185% of poverty guideline
   mutate(payment = ifelse(monthly_income > guidelines_month, 0, payment),
-         benefit = "Child care subsidy") %>%
+         benefit = "WIC") %>%
   select(composition, adults, children, monthly_income, payment, benefit)
 
-write_rds(care, 'benefits_tables/tables/child_care_subsidy.rds')
+write_rds(wic, 'benefits_tables/tables/wic.rds')
