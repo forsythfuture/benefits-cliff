1b8b16ea20442e8959f8e83d755ba1cde87d80b7
shanejorr <shane.j.orr@gmail.com>
2019-06-15 19:51:28 +0000

    update child care legend again

--- tax_liability/create_tax_outputs.R
+++ tax_liability/create_tax_outputs.R
@@ -46,7 +46,7 @@ sum_benefits <- function(df, benefit_name) {
 
 # sum benefits for all benefits, and for benefits without child care
 total_benefits <- map2(list(benefits, no_child_care),
-                       list("SNAP, TANF, Housing, WIC, Child Care", "SNAP, TANF, Housing, WIC"),
+                       list("Child care", "No child care"),
                        sum_benefits) %>%
   bind_rows() %>%
   ungroup()
