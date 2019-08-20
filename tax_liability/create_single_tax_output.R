056ca05b8f26c2322be7831552348d235146e26f
shanejorr <shane.j.orr@gmail.com>
2019-08-20 22:21:32 +0000

    add function that automatically creates scinerioes

--- tax_liability/create_single_tax_output.R
+++ tax_liability/create_single_tax_output.R
@@ -1,8 +1,9 @@
-#####################################################
+##################################################################################
 #
-# This script creates output for only taxable income
+# This script creates output for after tax income and each benefit.  The results
+# are sent to a json file for input into a java script visualization.
 #
-######################################################
+##################################################################################
 
 library(tidyverse)
 library(jsonlite)
@@ -72,7 +73,7 @@ difference <- master %>%
 # write out full dataset with hourly to an rds file
 write_rds(difference, "tax_liability/income_diff.rds")
 
-# write out condensed json dataset for plotting
+# write out condensed json dataset for plotting with java script
 difference %>%
   select(-hourly) %>%
   filter(pretax_inc <= 7500) %>%
