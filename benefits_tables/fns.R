4d753aa888f6c84af9149f1633892f7fa64be36e
shanejorr <shane.j.orr@gmail.com>
2019-10-24 03:10:12 +0000

    redo fns

--- benefits_tables/fns.R
+++ benefits_tables/fns.R
@@ -46,8 +46,8 @@ snap <- snap %>%
     # 20 percent of earned income is deducted,
     # so add column showing this amount
     ded_20 = monthly_income * .2,
-    # for dependent care deduction, assume $400 per child per month
-    dep_care = children * 400)
+    # for dependent care deduction, assume $600 per child per month
+    dep_care = children * 600)
 
 # calculate SNAP amounts
 snap <- snap %>%
