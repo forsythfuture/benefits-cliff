258f1fd4b402722f5f5bd908b637882712426c09
Daniel Ludolf <daniel@forsythfutures.org>
2021-10-04 11:06:15 -0400

    wic updated

--- Buncombe_County_2020/benefits_tables/wic.R
+++ Buncombe_County_2020/benefits_tables/wic.R
@@ -8,20 +8,16 @@ library(tidyverse)
 
 wic <- read_rds('Buncombe_County_2020/benefits_tables/tables/base.rds')
 
-# average benefit per person in 2018 was 42.28
-# https://fns-prod.azureedge.net/sites/default/files/resource-files/25wifyavgfd$-5.xls
-# assume recipients get this amount
-# to get benefits, the children must be under 5
-# we'll assume that all children are under 5 except
-# for one child in the 3 child house
-# the mother also gets benefits, and we'll assume that in one
-# adult households the adult is the mother
-# given this, the amount of the benefit is the number of children plus one,
-# times 42.28, minus 42.28 if there are three children
+# average benefit per person in 2021 was 28.08
+# https://www.fns.usda.gov/pd/wic-program go to Monthly Data – State Level Participation by Category and Program Costs: FY 2021 (preliminary)
+# assume recipients get this amount to get benefits, the children must be under 5 we'll assume that all 
+# children are under 5 except for one child in the 3 child house the mother also gets benefits, and we'll 
+# assume that in one adult households the adult is the mother given this, the amount of the benefit is 
+# the number of children plus one, times 28.08, minus 28.08 if there are three children
 wic <- wic %>%
   # only get WIC payment if family has kids
-  mutate(payment = ifelse(children > 0, (children + 1) * 42.28, 0),
-         payment = ifelse(children == 3, payment - 42.28, payment))
+  mutate(payment = ifelse(children > 0, (children + 1) * 28.08, 0),
+         payment = ifelse(children == 3, payment - 28.08, payment))
 
 # can receive wic up to 185% of fpl
 fpl <- read_rds('Buncombe_County_2020/benefits_tables/tables/federal_poverty_guidelines.rds') %>%
