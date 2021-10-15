a895a03f11c6df9c325ddb39a94332725527b7d2
Daniel Ludolf <daniel@forsythfutures.org>
2021-10-15 16:57:31 -0400

    clean up code

--- Buncombe_County_2020/income_cliff/cliff_population_2019.R
+++ Buncombe_County_2020/income_cliff/cliff_population_2019.R
@@ -1,3 +1,8 @@
+#############################################################################################################################################
+
+
+
+#############################################################################################################################################
 
 # read in data from google drive
 test <- read_csv("G://Shared drives/Forsyth Futures/Forsyth Futures Projects/JE_210001 Buncombe County Benefits Cliff Microsite/usa_00001.csv.gz")
@@ -16,7 +21,11 @@ test_2019 <- test %>%
          YEAR == 2019,
          MULTYEAR %in% 2015:2019)
 
-income_2019 <- incomes %>%
+#############################################################################################################################################
+
+# testing for 2019
+
+income_2019 <- test_2019 %>%
   # remove household incomes less than 0
   # we are interested in low-income / low net-worth people, and such people are unlikely
   # to have negative income
@@ -58,8 +67,11 @@ income_2019 <- incomes %>%
 
 write_csv(income, "Buncombe_County_2020/plots/cliff_cdf.csv")
 
+#############################################################################################################################################
 
-income_2019_test3 <- incomes %>%
+# 2019 Household size Buncombe County counts below/above income level
+
+income_2019_HH <- test_2019 %>%
   # remove household incomes less than 0
   # we are interested in low-income / low net-worth people, and such people are unlikely
   # to have negative income
@@ -105,16 +117,20 @@ income_2019_test3 <- incomes %>%
   select(size, HHINCOME, HHWT, total, FNS_threshold, indicator)
 
 #totals by household size
-totals_by_household_size <- income_2019_test3 %>%
+totals_by_household_size <- income_2019_HH %>%
   group_by(size, indicator) %>%
   summarise(sum = sum(total))
 
 #totals overall
-totals_overall <- income_2019_test3 %>%
+totals_overall <- income_2019_HH %>%
   group_by(indicator) %>%
   summarise(sum = sum(total))
 
-income_2019_test5 <- incomes %>%
+#############################################################################################################################################
+
+# 2019 FNS Buncombe County counts
+
+income_2019_fns <- test_2019 %>%
   # remove household incomes less than 0
   # we are interested in low-income / low net-worth people, and such people are unlikely
   # to have negative income
@@ -160,30 +176,39 @@ income_2019_test5 <- incomes %>%
   ungroup() %>%
   select(size, food_stamps, HHINCOME, total, FNS_threshold, indicator)
 
-#totals by household size
-totals_by_food_stamps<- income_2019_test5 %>%
+#totals by food stamp status
+totals_by_fns<- income_2019_fns %>%
   group_by(food_stamps) %>% 
   summarise(sum = sum(total))
 
+#############################################################################################################################################
 
-boo <- testing_test3 %>%
-  filter(size == 3)
+# 2019 race/ethnicity Buncombe County counts below/above income level
 
-boo2 <- income_2019_test3 %>%
-  filter(size == 3)
+# boo <- testing_test3 %>%
+#   filter(size == 3)
+# 
+# boo2 <- income_2019_test3 %>%
+#   filter(size == 3)
+# 
+# sum(boo$officialHHWT)
+# sum(boo2$HHWT)
 
-sum(boo$officialHHWT)
-sum(boo2$HHWT)
-
-#4463845
-
-testing_test <- incomes %>%
+income_2019_race <- test_2019 %>%
+  #race/ethnicity recode
   mutate(race_ethnicity = if_else(between(HISPAN,1,4), "Hispanic/Latino", 
                                   if_else((HISPAN == 0 | HISPAN == 9) & RACE == 1, "White, NH", 
                                           if_else((HISPAN == 0 | HISPAN == 9) & RACE == 2, "Black/AA, NH", 
                                                   if_else((HISPAN == 0 | HISPAN == 9) & (RACE != 1 | RACE != 2), "Other Race", NA_character_))))) %>%
+  # remove household incomes less than 0
+  # we are interested in low-income / low net-worth people, and such people are unlikely
+  # to have negative income
   filter(HHINCOME >= 0) %>%
+  # create boolean of whether person is in school
+  # we'll later remove families where everyone is in school,
+  # because such families would generally not be entitled to benefits
   mutate(in_school = ifelse(SCHOOL == 2, TRUE, FALSE)) %>%
+  # group by household
   group_by(SERIAL) %>%
   mutate(size = n(), # household size
          total_school= sum(in_school), # total number in school
@@ -193,19 +218,31 @@ testing_test <- incomes %>%
   ) %>%
   select(SERIAL, size, HHINCOME, HHWT, race_ethnicity)
 
+# function to pull SERIAL #s mentioned 2+ times
 myFun <- function(vector, thresh) {
   ind <- ave(rep(1, length(vector)), vector, FUN = length)
   vector[ind > thresh]
 }
 
-bang <- myFun(incomes$SERIAL, 1) %>% unique()
+# vector of serial numbers mentioned at least 2+ times
+multiple_serials <- myFun(incomes$SERIAL, 1) %>% unique()
 
-testing_test3 <- testing_test %>%
-  mutate(newHHWT = if_else(SERIAL %in% bang, HHWT / size, HHWT)) %>%
+#pick up the recode
+income_2019_race2 <- income_2019_race %>%
+  #make a new HHWT variable that divides the HHWT by the size of the household to get the individual weight
+  mutate(newHHWT = if_else(SERIAL %in% multiple_serials, HHWT / size, HHWT)) %>%
   group_by(size, HHINCOME, race_ethnicity) %>%
+  # now group by size and income and race
+  # we're grouping by income because we will sum household weights by income and race
+  # so they are aggregated and we don't have multiple rows of the same income
+  # sum newHHWT
   summarize(officialHHWT = sum(newHHWT)) %>%
   arrange(size, HHINCOME, race_ethnicity) %>%
+  # remove household incomes less than 72000, which is a monthly income of 6000
+  # this is the amount we use for our other charts
   filter(HHINCOME <= 72000) %>%
+  # the y axis of the plot should reflect number of people, not number of households,
+  # so, multiply HHWT by size to convert number of households to number of people
   mutate(total = size * officialHHWT) %>%
   ungroup() %>%
   group_by(size) %>%
@@ -221,10 +258,9 @@ testing_test3 <- testing_test %>%
   ungroup() %>%
   select(size, race_ethnicity, HHINCOME, officialHHWT, total, FNS_threshold, indicator)
   
-
-totals_by_race_ethnicity <- testing_test3 %>%
-    group_by(race_ethnicity, indicator) %>% 
-    # filter(race_ethnicity != "Other Race") %>%
+#totals by race/ethnicity
+totals_by_race_ethnicity <- income_2019_race2 %>%
+    group_by(race_ethnicity, indicator) %>%
     summarise(sum = sum(total))
   
   
