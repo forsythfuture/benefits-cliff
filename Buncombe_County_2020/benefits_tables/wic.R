142dd5f576d27e37d62530cf3b689b7fb9f60d50
Daniel Ludolf <daniel@forsythfutures.org>
2021-10-18 16:31:37 -0400

    updated wic

--- Buncombe_County_2020/benefits_tables/wic.R
+++ Buncombe_County_2020/benefits_tables/wic.R
@@ -8,16 +8,16 @@ library(tidyverse)
 
 wic <- read_rds('Buncombe_County_2020/benefits_tables/tables/base.rds')
 
-# average benefit per person in 2021 was 28.08
+# average benefit per person in 2021 was 31.06
 # https://www.fns.usda.gov/pd/wic-program go to Monthly Data – State Level Participation by Category and Program Costs: FY 2021 (preliminary)
 # assume recipients get this amount to get benefits, the children must be under 5 we'll assume that all 
 # children are under 5 except for one child in the 3 child house the mother also gets benefits, and we'll 
 # assume that in one adult households the adult is the mother given this, the amount of the benefit is 
-# the number of children plus one, times 28.08, minus 28.08 if there are three children
+# the number of children plus one, times 31.06, minus 31.06 if there are three children
 wic <- wic %>%
   # only get WIC payment if family has kids
-  mutate(payment = ifelse(children > 0, (children + 1) * 28.08, 0),
-         payment = ifelse(children == 3, payment - 28.08, payment))
+  mutate(payment = ifelse(children > 0, (children + 1) * 31.06, 0),
+         payment = ifelse(children == 3, payment - 31.06, payment))
 
 # can receive wic up to 185% of fpl
 fpl <- read_rds('Buncombe_County_2020/benefits_tables/tables/federal_poverty_guidelines.rds') %>%
