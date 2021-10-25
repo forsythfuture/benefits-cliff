b0466eb370be139761a3264874413e71dda420c9
Daniel Ludolf <daniel@forsythfutures.org>
2021-10-25 11:02:08 -0400

    updated html graphs with FNS numbers

--- Buncombe_County_2020/income_cliff/cliff_population_2019.R
+++ Buncombe_County_2020/income_cliff/cliff_population_2019.R
@@ -135,6 +135,50 @@ totals_overall <- income_2019_HH %>%
 
 #############################################################################################################################################
 
+# 2019 Household size Buncombe County counts by foodstamp status
+
+income_2019_HH_foodstamps <- test_2019 %>%
+  # remove household incomes less than 0
+  # we are interested in low-income / low net-worth people, and such people are unlikely
+  # to have negative income
+  filter(HHINCOME >= 0) %>%
+  # create boolean of whether person is in school
+  # we'll later remove families where everyone is in school,
+  # because such families would generally not be entitled to benefits
+  mutate(in_school = ifelse(SCHOOL == 2, TRUE, FALSE)) %>%
+  # group by household
+  group_by(SERIAL) %>%
+  mutate(size = n(), # household size
+         total_school= sum(in_school), # total number in school
+  ) %>%
+  filter(size != total_school, # remove households where all people are in school
+         size <= 5, # only keep households with 5 or fewer people, for plotting
+  ) %>%
+  select(SERIAL, HHWT, HHINCOME, size, FOODSTMP) %>%
+  distinct() %>%
+  # now group by size and income
+  # we're grouping by income because we will sum household weights by income
+  # so they are aggregated and we don't have multiple rows of the same income
+  group_by(size, HHINCOME, FOODSTMP) %>%
+  summarize(HHWT = sum(HHWT)) %>%
+  arrange(size, HHINCOME) %>%
+  # remove household incomes less than 72000, which is a monthly income of 6000
+  # this is the amount we use for our other charts
+  filter(HHINCOME <= 72000) %>%
+  # the y axis of the plot should reflect number of people, not number of households,
+  # so, multiply HHWT by size to convert number of households to number of people
+  mutate(total = size * HHWT) %>%
+  ungroup() %>%
+  select(size, HHINCOME, HHWT, total, FOODSTMP)
+
+#totals by household size by foodstamp status
+totals_by_household_size_foodstamps <- income_2019_HH_foodstamps %>%
+  group_by(size, FOODSTMP) %>%
+  summarise(sum = sum(total))
+
+
+#############################################################################################################################################
+
 # 2019 FNS Buncombe County counts
 
 income_2019_fns <- test_2019 %>%
@@ -269,5 +313,54 @@ income_2019_race2 <- income_2019_race %>%
 totals_by_race_ethnicity <- income_2019_race2 %>%
     group_by(race_ethnicity, indicator) %>%
     summarise(sum = sum(total))
-  
-  
+
+#############################################################################################################################################
+
+# 2019 race/ethnicity Buncombe County counts by foodstamp status
+# the myFun function in the script above needs to be ran first before running this code
+
+income_2019_race_foodstamps <- test_2019 %>%
+  #race/ethnicity recode
+  mutate(race_ethnicity = if_else(between(HISPAN,1,4), "Hispanic/Latino", 
+                                  if_else((HISPAN == 0 | HISPAN == 9) & RACE == 1, "White, NH", 
+                                          if_else((HISPAN == 0 | HISPAN == 9) & RACE == 2, "Black/AA, NH", 
+                                                  if_else((HISPAN == 0 | HISPAN == 9) & (RACE != 1 | RACE != 2), "Other Race", NA_character_))))) %>%
+  # remove household incomes less than 0
+  # we are interested in low-income / low net-worth people, and such people are unlikely
+  # to have negative income
+  filter(HHINCOME >= 0) %>%
+  # create boolean of whether person is in school
+  # we'll later remove families where everyone is in school,
+  # because such families would generally not be entitled to benefits
+  mutate(in_school = ifelse(SCHOOL == 2, TRUE, FALSE)) %>%
+  # group by household
+  group_by(SERIAL) %>%
+  mutate(size = n(), # household size
+         total_school= sum(in_school), # total number in school
+  ) %>%
+  filter(size != total_school, # remove households where all people are in school
+         size <= 5, # only keep households with 5 or fewer people, for plotting
+  ) %>%
+  select(SERIAL, size, HHINCOME, HHWT, race_ethnicity, FOODSTMP) %>%
+  #make a new HHWT variable that divides the HHWT by the size of the household to get the individual weight
+  mutate(newHHWT = if_else(SERIAL %in% multiple_serials, HHWT / size, HHWT)) %>%
+  group_by(size, HHINCOME, race_ethnicity, FOODSTMP) %>%
+  # now group by size and income and race
+  # we're grouping by income because we will sum household weights by income and race
+  # so they are aggregated and we don't have multiple rows of the same income
+  # sum newHHWT
+  summarize(officialHHWT = sum(newHHWT)) %>%
+  arrange(size, HHINCOME, race_ethnicity) %>%
+  # remove household incomes less than 72000, which is a monthly income of 6000
+  # this is the amount we use for our other charts
+  filter(HHINCOME <= 72000) %>%
+  # the y axis of the plot should reflect number of people, not number of households,
+  # so, multiply HHWT by size to convert number of households to number of people
+  mutate(total = size * officialHHWT) %>%
+  ungroup() %>%
+  select(size, race_ethnicity, HHINCOME, officialHHWT, total, FOODSTMP)
+
+#totals by race/ethnicity by foodstamp status
+totals_by_race_ethnicity_foodstamps <- income_2019_race_foodstamps %>%
+  group_by(race_ethnicity, FOODSTMP) %>%
+  summarise(sum = sum(total))
