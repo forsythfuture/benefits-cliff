51c09c9db22bf2ed03dd8e00711b4bcf0b1b8f0f
Daniel Ludolf <daniel@forsythfutures.org>
2022-01-21 16:11:39 -0500

    updated FPG numbers for 2022 and dropped 2019, and edited grammar errors in income_diff graph

--- Buncombe_County_2020/benefits_tables/federal_poverty_guidelines.R
+++ Buncombe_County_2020/benefits_tables/federal_poverty_guidelines.R
@@ -6,16 +6,16 @@
 
 library(tidyverse)
 
-# federal poverty guidelines for 2019, 2020, 2021 ---------------------------
+# federal poverty guidelines for 2020, 2021, 2022 ---------------------------
 
+# 2022: https://aspe.hhs.gov/topics/poverty-economic-mobility/poverty-guidelines
 # 2021: https://aspe.hhs.gov/topics/poverty-economic-mobility/poverty-guidelines/prior-hhs-poverty-guidelines-federal-register-references/2021-poverty-guidelines
 # 2020: https://aspe.hhs.gov/topics/poverty-economic-mobility/poverty-guidelines/prior-hhs-poverty-guidelines-federal-register-references/2020-poverty-guidelines
-# 2019: https://aspe.hhs.gov/topics/poverty-economic-mobility/poverty-guidelines/prior-hhs-poverty-guidelines-federal-register-references/2019-poverty-guidelines
 
 fpg <- data.frame(household_size = rep(seq_len(8), times = 3),
-                    guidelines_year = c(12880, 17420, 21960, 26500, 31040, 35580, 40120, 44660,
-                                        12760, 17240, 21720, 26200, 30680, 35160, 39640, 44120,
-                                        12490, 16910, 21330, 25750, 30170, 34590, 39010, 43430),
+                    guidelines_year = c(13590, 18310, 23030, 27750, 32470, 37190, 41910, 46630,
+                                        12880, 17420, 21960, 26500, 31040, 35580, 40120, 44660,
+                                        12760, 17240, 21720, 26200, 30680, 35160, 39640, 44120),
                     year = rep(c(2021, 2020, 2019), each = 8)) %>%
     # add montly guidelines
     mutate(guidelines_month = round(guidelines_year / 12, 0)) %>%
